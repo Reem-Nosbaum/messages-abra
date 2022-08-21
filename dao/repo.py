82cd6deeaac184c964dc0b4ab27c0cf45213296b
reemnu <reemnu@gmail.com>
2022-08-21 17:17:08 +0300

    make it better

---
+++ dao/repo.py
@@ -0,0 +1,19 @@
+from run import Users
+from run import Messages
+from werkzeug.security import check_password_hash, generate_password_hash
+from run import db
+import uuid
+
+
+class Repo:
+
+    @staticmethod
+    def get_user_by_username(username: str) -> list[Users]:
+        return Users.query.filter_by(username=username).all()
+
+    @staticmethod
+    def create_new_user(username: str, password: str) -> None:
+        hashed_password = generate_password_hash(password, method='sha256')  # hashing the password
+        new_user = Users(public_id=str(uuid.uuid4()), username=username, password=hashed_password)
+        db.session.add(new_user)  # adding the new user to the db
+        db.session.commit()
